8247b7f49355290886045edc556cb42f820537bf
rmpestano <rmpestano@gmail.com>
2017-11-04 21:25:24 -0200

    perf

--- run.sh
+++ run.sh
@@ -1,4 +1,2 @@
 #!/bin/sh
-#docker run -it --privileged -d docker:dind --name jenkins-last-changes -p 8080:8080 -v /var/jenkins_home rmpestano/jenkins-last-changes
-
-docker run -it --name jenkins-last-changes -p 8080:8080 -v "$HOME/.m2":/root/.m2 -v /var/run/docker.sock:/var/run/docker.sock -v $(which docker):/usr/bin/docker -v /var/jenkins_home rmpestano/jenkins-last-changes
+docker run -it --name jenkins -p 8080:8080 -v "$HOME/.m2":/root/.m2 -v /var/run/docker.sock:/var/run/docker.sock -v $(which docker):/usr/bin/docker --privileged -v ~/.jenkins:/var/jenkins_home rmpestano/jenkins
